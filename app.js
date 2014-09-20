ac9d2de5a2d7b13177090f20f05f829840183c53
SomeKittens <rkoutnik@gmail.com>
2014-09-20 12:57:27 -0700

    dat gui and exploding collisions

--- app.js
+++ app.js
@@ -7,9 +7,43 @@ app.http().io();
 
 app.use(express.static(__dirname));
 
-['join', 'exploded', 'reset', 'heartbeat', 'leave'].forEach(function (e) {
+var consts = {
+  shipSize: 36
+};
+
+var players = {};
+
+function within(x, y, within) {
+  return x + within >= y && x - within <= y;
+}
+
+['exploded', 'reset', 'leave'].forEach(function (e) {
   app.io.route(e, function (req) {
     req.io.broadcast(e, req.data);
+  });
+});
+
+app.io.route('join', function(req) {
+  req.io.broadcast('join', req.data);
+  players[req.data.name] = req.data;
+});
+
+app.io.route('heartbeat', function(req) {
+  req.io.broadcast('join', req.data);
+  players[req.data.name] = req.data;
+
+  // Collision check
+  Object.keys(players).forEach(function(key) {
+    if (key === req.data.name) { return; }
+    var otherPlayer = players[key];
+    var x = otherPlayer.x - req.data.x,
+        y = otherPlayer.y - req.data.y,
+        distance = Math.sqrt(x*x + y*y);
+    if (distance <= consts.shipSize) {
+      console.log(req.data.name, ' collided with ', key);
+      app.io.broadcast('exploded', {name: key});
+      app.io.broadcast('exploded', {name: req.data.name});
+    }
   });
 });
 
---
+++ dat.spaceships.js
@@ -0,0 +1,4 @@
+var gui = new dat.GUI();
+
+var gPlayer = gui.addFolder('Player');
+gPlayer.add(thisPlayer, 'thrust');
