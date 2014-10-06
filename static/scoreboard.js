1d2b02b0327ef460e7b41e6da999a34c611122f3
SomeKittens <rkoutnik@gmail.com>
2014-10-05 20:02:09 -0700

    fixed scoreboard size on long names

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -9,16 +9,26 @@ inherits(Scoreboard, CanvasItem);
 
 Scoreboard.prototype.render = function () {
   var row = 0;
+  var longest = {
+    name: ''
+  };
+  if (this.scores) {
+    longest = this.scores.sort(function(a, b) {
+      return b.name.length - a.name.length;
+    })[0];
+  }
   context.fillStyle = 'white';
   context.font = '18px Arial';
-  context.fillText('Name', width - 150, 20);
+  var offset = Math.max(context.measureText(longest.name).width, 150);
+
+  context.fillText('Name', width - offset, 20);
   context.fillText('Score', width - 75, 20);
   if (this.scores) {
     row++;
     context.font = '14px Arial';
     this.scores.forEach(function(score) {
       row++;
-      context.fillText(score.name, width - 150, row * 20);
+      context.fillText(score.name, width - offset, row * 20);
       context.fillText(score.score, width - 75, row * 20);
     });
   }
